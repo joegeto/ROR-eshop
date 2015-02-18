439f3cf02f13d69ec72c75442e1f630d1a98e6f7
Sulman Baig <sulman@sulmanbaig.com>
2015-02-18 15:55:27 +0100

    Adding images to the thumbnails and adding books to index page

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Eshop
 
     config.paperclip_defaults = {
         storage: :s3,
+        s3_host_name: "s3.eu-central-1.amazonaws.com",
         s3_credentials: {
             bucket: ENV['AWS_BUCKET'],
             access_key_id: ENV['AWS_ACCESS_KEY_ID'],
